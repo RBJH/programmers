1e8dfd2c1d23fbbdc22abb625fbe8ff97cfde270
RBJH <46277703+RBJH@users.noreply.github.com>
2019-10-10 23:03:58 +0900

    Solve 정수 제곱근 판별

---
+++ Level 1/정수 제곱근 판별.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+  long long answer = 0;
+
+  long long x = sqrt(n);
+
+  if (x * x == n)
+    answer = (x + 1) * (x + 1);
+  else
+    answer = -1;
+
+  return answer;
+}
