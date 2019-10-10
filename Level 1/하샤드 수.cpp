cbe361b5bd9968104c3792e9f899079d840ac77d
RBJH <46277703+RBJH@users.noreply.github.com>
2019-10-10 23:39:15 +0900

    Solve 하샤드 수

---
+++ Level 1/하샤드 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+  bool answer = true;
+
+  int tmp = x;
+
+  int sum = 0;
+
+  while (tmp) {
+    sum += tmp % 10;
+
+    tmp /= 10;
+  }
+
+  if (x % sum) answer = false;
+
+  return answer;
+}
