b74e899a9a21706a08649b12d717a502c150fc1a
JUNHWAN YUN <j@JUNHWANs-MacBook-Pro.local>
2019-10-12 17:37:01 +0900

    Solve 주식가격

---
+++ 코딩테스트 고득점 Kit/스택 큐/주식가격.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    int cnt = 0;
+    
+    for(int i = 0; i < prices.size(); i++) {
+        cnt = 0;
+        
+        for(int j = i + 1; j < prices.size(); j++) {
+            cnt++;
+            
+            if(prices[j] < prices[i]) break;
+        }    
+        
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
